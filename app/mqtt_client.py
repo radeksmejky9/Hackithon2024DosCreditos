9cb9e95c5484c047302ceba75765d724cf5d55a4
Helmanz <helcze4@gmail.com>
2024-05-25 03:01:49 +0200

    dash main graph

--- app/mqtt_client.py
+++ app/mqtt_client.py
@@ -10,34 +10,48 @@ MQTT_BROKER = os.getenv("MQTT_BROKER")
 MQTT_TOPIC = os.getenv("MQTT_TOPIC")
 MQTT_USER = os.getenv("MQTT_USER")
 MQTT_PASSWORD = os.getenv("MQTT_PASSWORD")
-mqtt_data = []
+TOPICS = {
+    "ttndata": "ttndata",
+    "senzory": "Senzory - wifi",
+    "mve": "mve",
+    "Bilina": "Bílina - kamery",
+    "vodomery": "Děčín - vodoměry",
+    "udp1881": "UDP 1881",
+    "unknown": "Neznámé",
+}
+
+mqtt_data = {value: [] for value in TOPICS.keys()}
 
 
 def on_connect(client, userdata, flags, reason_code, properties):
-    print(f"Connected with result code {reason_code}")
     client.subscribe(MQTT_TOPIC)
 
 
 def on_message(client, userdata, msg):
-    try:
-        payload = msg.payload.decode("utf-8")
-        try:
-            json_payload = json.loads(payload)
-            json_bytes = json.dumps(json_payload).encode("utf-8")
-            json_size = len(json_bytes)
-            print(f"{msg.topic} - JSON payload size: {json_size} bytes")
+    json_size = get_msg_size(msg)
+    if json_size == -1:
+        return
 
-            mqtt_data.append(
-                {
-                    "topic": msg.topic,
-                    "size": json_size,
-                    "timestamp": datetime.datetime.now(),
-                }
-            )
-        except json.JSONDecodeError:
-            print("Payload is not in JSON format")
-    except UnicodeDecodeError:
-        print("Failed to decode payload with UTF-8")
+    topic = msg.topic.strip("/").split("/")[0]
+
+    if topic in TOPICS.keys():
+        mqtt_data[topic].append(
+            {
+                "topic": msg.topic,
+                "topic_readable": TOPICS[topic],
+                "size": json_size,
+                "timestamp": datetime.datetime.now(),
+            }
+        )
+    else:
+        mqtt_data["unknown"].append(
+            {
+                "topic": msg.topic,
+                "topic_readable": TOPICS["unknown"],
+                "size": json_size,
+                "timestamp": datetime.datetime.now(),
+            }
+        )
 
 
 def run_mqtt_client():
@@ -49,3 +63,19 @@ def run_mqtt_client():
 
     mqttc.connect(MQTT_BROKER, 8883, 60)
     mqttc.loop_forever()
+
+
+def get_msg_size(msg):
+    try:
+        payload = msg.payload.decode("utf-8")
+    except UnicodeDecodeError:
+        print("Failed to decode payload with UTF-8")
+        return -1
+
+    try:
+        json_payload = json.loads(msg.payload)
+        json_bytes = json.dumps(json_payload).encode("utf-8")
+        return len(json_bytes)
+    except json.JSONDecodeError:
+        print("Payload is not in JSON format")
+        return -1
--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,12 @@
 from app import app
 from flask import render_template
 from app.mqtt_client import mqtt_data
-from dash import dcc, html, Input, Output
+from app.mqtt_client import TOPICS
+from dash import dcc, html, Input, Output, State
 
 layout = html.Div(
     [
-        html.H1("MQTT Data"),
+        html.H1("MQTT Data flow"),
         dcc.Graph(id="mqtt_graph"),
         dcc.Interval(
             id="interval-component", interval=1000, n_intervals=0  # in milliseconds
@@ -16,28 +17,35 @@ app.layout = layout
 
 
 @app.callback(
-    Output("mqtt_graph", "figure"), [Input("interval-component", "n_intervals")]
+    Output("mqtt_graph", "figure"),
+    [Input("interval-component", "n_intervals")],
+    [State("mqtt_graph", "relayoutData")],
 )
-def update_graph(n):
-    # Process mqtt_data to create a graph figure
-    x = [data["timestamp"] for data in mqtt_data]
-    y = [data["size"] for data in mqtt_data]
-    topics = [data["topic"] for data in mqtt_data]
-
+def update_graph(n, relayoutData):
     traces = []
-    for topic in set(topics):
-        x_topic = [x[i] for i in range(len(x)) if topics[i] == topic]
-        y_topic = [y[i] for i in range(len(y)) if topics[i] == topic]
-        traces.append(dict(x=x_topic, y=y_topic, mode="markers+lines", name=topic))
+    for topic_key in TOPICS.keys():
+        traces.append(dict(x=[], y=[], mode="markers+lines", name=topic_key))
 
-    return {
-        "data": traces,
-        "layout": {
-            "title": "MQTT Data",
-            "xaxis": {"title": "Timestamp"},
-            "yaxis": {"title": "Size"},
-        },
+    # Update traces with available data
+    for topic, data_list in mqtt_data.items():
+        topic_index = list(TOPICS.keys()).index(topic)
+        for data in data_list:
+            traces[topic_index]["x"].append(data["timestamp"])
+            traces[topic_index]["y"].append(data["size"])
+
+    layout = {
+        "title": "MQTT Data",
+        "xaxis": {"title": "Timestamp"},
+        "yaxis": {"title": "Size"},
     }
+
+    if relayoutData:
+        if "xaxis.range" in relayoutData:
+            layout["xaxis"]["range"] = relayoutData["xaxis.range"]
+        if "yaxis.range" in relayoutData:
+            layout["yaxis"]["range"] = relayoutData["yaxis.range"]
+
+    return {"data": traces, "layout": layout}
 
 
 @app.server.route("/")
