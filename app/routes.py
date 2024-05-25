3839e225ff8af483902073019b3c2492244a2095
Helmanz <helcze4@gmail.com>
2024-05-25 08:41:02 +0200

    req per min

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,12 @@
-from collections import Counter
+from collections import Counter, deque
 from app import app, redisClient
 from flask import render_template
-from app.mqtt_client import TOPICS
+from app.mqtt_client import TOPICS, message_count
 from dash import dcc, html, Input, Output
+import datetime
+
+start_time = datetime.datetime.now()
+messages_per_minute_history = deque(maxlen=50)
 
 layout = html.Div(
     [
@@ -27,6 +31,7 @@ layout = html.Div(
         ),
         dcc.Graph(id="top_size_graph"),
         dcc.Graph(id="top_transfers_graph"),
+        dcc.Graph(id="req_per_min_graph"),
         dcc.Interval(
             id="interval-component", interval=1000, n_intervals=0  # in milliseconds
         ),
@@ -149,6 +154,43 @@ def update_top_transfers_graph(n):
         "layout": layout,
     }
     return top_transfers_figure
+
+
+@app.callback(
+    Output("req_per_min_graph", "figure"),
+    [Input("interval-component", "n_intervals")],
+)
+def update_req_per_min_graph(n):
+    global messages_per_minute_history
+
+    layout = {
+        "title": "Messages per Minute",
+        "xaxis": {"title": "Time"},
+        "yaxis": {"title": "Messages per Minute"},
+    }
+    data = get_data(layout=layout)
+    current_time = datetime.datetime.now()
+    elapsed_time = (current_time - start_time).total_seconds()
+
+    if elapsed_time > 0:
+        messages_per_minute = len(data) / (elapsed_time / 60)
+
+    messages_per_minute_history.append([current_time, messages_per_minute])
+
+    req_per_min_figure = {
+        "data": [
+            {
+                "x": [record[0] for record in messages_per_minute_history],
+                "y": [record[1] for record in messages_per_minute_history],
+                "type": "scatter",
+                "mode": "lines",
+                "name": "Messages per Minute",
+            }
+        ],
+        "layout": layout,
+    }
+
+    return req_per_min_figure
 
 
 # @app.callback(
